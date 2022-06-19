2f581496597b411fde275769c336ad0b99389732
Ennio-M <marrese.ennio@gmail.com>
2022-06-19 03:02:14 +0200

    model, controller, recupero dati e passaggio view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -94,6 +94,11 @@
                     <a href="https://vapor.laravel.com">Vapor</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
+                <ul>
+                    @foreach ($movies as $movie)
+                        <li>{{$movie->title}}</li>
+                    @endforeach
+                </ul>
             </div>
         </div>
     </body>
